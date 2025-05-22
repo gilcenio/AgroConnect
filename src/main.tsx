a7147a1a6796ca0ad4cf62f62b6417e457d2a8ef
gilceniosantos <gilcenio@1mage.com>
2025-05-22 01:12:46 -0300

    Ajustes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
-import Teste from './teste';
+import Routes from './Routes/index';
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
-    {/* <StyledEngineProvider injectFirst> */}
-      <Teste />
-    {/* </StyledEngineProvider> */}
+    <StyledEngineProvider injectFirst>
+      <Routes />
+    </StyledEngineProvider>
   </React.StrictMode>
 );
